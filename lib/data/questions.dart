2d69b53c7ddc48a3a5bfc18062481f1ee6bf281c
HaithamGh <Haithammghaith@gmail.com>
2024-01-06 21:10:59 +0300

    sample commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,73 @@
+import 'package:adv_basics/models/quiz_question.dart';
+//import 'package:udemyquiz/models/quiz-questions.dart';
+
+const questions = [
+
+  
+  QuizQuestion(//q1
+    'What\' my major?',
+        [
+          'Computer Science',
+          'Computer Eng',
+          'Data Science',
+          'Artifical Intelligence',
+        ],
+  ),
+
+
+  QuizQuestion(//q2
+    'When is my birthday?',
+        [
+        '7th of June 2003',
+        '1st of Jan 2003',
+        '6th of Jul 2003',
+        '23th of Aug 2003',
+        ]
+  ),
+
+
+  QuizQuestion(//q3
+    'How tall am i?',
+        [
+          '180 cm',
+          '186 cm',
+          '173 cm',
+          '189 cm',
+        ],
+  ),
+
+
+  QuizQuestion(//q4
+    'Which programming language am I most proficient in?',
+        [
+          'C++',
+          'Python',
+          'JavaScript',
+          'Java',
+        ],
+  ),
+
+
+  QuizQuestion(//q5
+    'What is my favorite hobby?',
+        [
+          'Reading',
+          'Gaming',
+          'Playing sports',
+          'Drawing/Painting',
+        ],
+  ),
+
+
+  QuizQuestion(//q6
+    'Which technology am I most interested in learning next?',
+        [
+          'Internet of Things (IoT)',
+          'Machine Learning',
+          'Blockchain',
+          'AR/VR',
+        ],
+  ),
+
+
+];//end of questions
